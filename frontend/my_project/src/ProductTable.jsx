7e485f64898f0014925412301771b559afbb4378
chaitanya225-11 <s.chaitany.sai@gmail.com>
2026-06-14 15:24:12 +0530

    Update ProductTable.jsx

--- frontend/my_project/src/ProductTable.jsx
+++ frontend/my_project/src/ProductTable.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./ProductTable.css";
+import "./productTable.css";
 
 function ProductTable({ editingProduct, setEditingProduct }) {
   const [products, setProducts] = useState([]);
